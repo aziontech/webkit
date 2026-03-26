6587dc2e1cb5f1cb67025f51e0d8341d19607c5e
gab-az <gabriel.mendonca@azion.com.br>
2026-03-26 00:30:14 -0300

    fix: iconPos and data-testid param

--- apps/storybook/src/stories/components/buttons/ButtonCancel.stories.js
+++ apps/storybook/src/stories/components/buttons/ButtonCancel.stories.js
@@ -13,6 +13,11 @@ export default {
       control: 'text',
       description: 'Icon class (PrimeIcons format)'
     },
+    'iconPos': {
+      control: 'select',
+      options: ['left', 'right'],
+      description: 'Position of the icon (left or right)'
+    },
     severity: {
       control: 'select',
       options: ['', 'secondary', 'success', 'info', 'warning', 'danger', 'contrast'],
@@ -45,6 +50,15 @@ export const WithIcon = {
   args: {
     label: 'Cancel',
     icon: 'pi pi-times',
+    size: 'large'
+  }
+};
+
+export const IconRight = {
+  args: {
+    label: 'Cancel',
+    icon: 'pi pi-times',
+    'iconPos': 'right',
     size: 'large'
   }
 };
