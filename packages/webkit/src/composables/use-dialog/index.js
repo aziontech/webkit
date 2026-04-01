ee797da93bb8c957fdb1848b9917ab24dd097d04
gab-az <gabriel.mendonca@azion.com.br>
2026-04-01 18:12:25 -0300

    feat(primevue): add abstraction layer for PrimeVue services, composables, and plugin (ENG-37137)
    - Add useToast wrapper composable with semantic shortcuts (success, error, warn, info)
    - Add useDialog wrapper composable
    - Add FilterMatchMode re-export via api entry point
    - Add DynamicDialog and ConfirmDialog wrapper components
    - Add Tooltip directive re-export
    - Add WebkitPlugin that registers PrimeVue, ToastService, DialogService, and Tooltip
    - Update package.json with 7 new exports
    - Add abstraction approaches documentation

---
+++ packages/webkit/src/composables/use-dialog/index.js
@@ -0,0 +1,5 @@
+import { useDialog as usePrimeDialog } from 'primevue/usedialog'
+
+export function useDialog() {
+  return usePrimeDialog()
+}
---
+++ packages/webkit/src/core/primevue/confirm-dialog/confirm-dialog.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ConfirmDialog from 'primevue/confirmdialog'
+
+defineOptions({ name: 'ConfirmDialog' })
+
+const props = defineProps({
+  group: {
+    type: String,
+    default: undefined
+  },
+  breakpoints: {
+    type: Object,
+    default: undefined
+  }
+})
+</script>
+
+<template>
+  <ConfirmDialog
+    :group="props.group"
+    :breakpoints="props.breakpoints"
+  >
+    <template v-if="$slots.message" #message="slotProps">
+      <slot name="message" v-bind="slotProps" />
+    </template>
+  </ConfirmDialog>
+</template>
