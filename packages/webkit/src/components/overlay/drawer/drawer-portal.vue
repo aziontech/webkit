45407123e7506d12a1aa9cc481c72eafb929cb70
gab-az <gabriel.mendonca@azion.com.br>
2026-05-25 18:46:12 -0300

    feat(webkit): add drawer overlay component

---
+++ packages/webkit/src/components/overlay/drawer/drawer-portal.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  import { computed, inject, onBeforeUnmount, ref, watch } from 'vue'
+
+  import { DrawerInjectionKey } from './injection-key'
+  import { DRAWER_EXIT_MS } from './presets/transitions'
+
+  defineOptions({
+    name: 'DrawerPortal',
+    inheritAttrs: false
+  })
+
+  defineSlots<{
+    default(): unknown
+  }>()
+
+  const ctx = inject(DrawerInjectionKey)
+  const isOpen = computed(() => ctx?.isOpen.value ?? false)
+  const isPresent = ref(isOpen.value)
+  let exitTimer: ReturnType<typeof setTimeout> | undefined
+
+  watch(isOpen, (open) => {
+    if (exitTimer) {
+      clearTimeout(exitTimer)
+      exitTimer = undefined
+    }
+
+    if (open) {
+      isPresent.value = true
+      return
+    }
+
+    if (!isPresent.value) return
+
+    exitTimer = setTimeout(() => {
+      isPresent.value = false
+      exitTimer = undefined
+    }, DRAWER_EXIT_MS)
+  })
+
+  onBeforeUnmount(() => {
+    if (exitTimer) clearTimeout(exitTimer)
+  })
+</script>
+
+<template>
+  <Teleport
+    v-if="isPresent"
+    to="body"
+  >
+    <slot />
+  </Teleport>
+</template>
---
+++ packages/webkit/src/components/overlay/drawer/injection-key.js
@@ -0,0 +1,1 @@
+export const DrawerInjectionKey = Symbol('DrawerContext')
