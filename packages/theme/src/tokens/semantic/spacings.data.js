d28e90e421e0abed24c6e42456dcf4444ded763b
Isaque Böck <isaquebockdos@gmail.com>
2026-05-25 11:10:34 -0300

    fix: rename spacing-elements-* tokens to spacing-*

--- packages/theme/src/tokens/semantic/spacings.data.js
+++ packages/theme/src/tokens/semantic/spacings.data.js
@@ -6,13 +6,13 @@
  */
 
 export const spacingsData = {
-  'spacing-elements-xxl': { _: '2rem', sm: '4rem', xl: '6rem' },
-  'spacing-elements-xl': { _: '1.5rem', sm: '2rem', xl: '3rem' },
-  'spacing-elements-lg': { _: '1rem', sm: '1.5rem' },
-  'spacing-elements-md': { _: '1rem' },
-  'spacing-elements-sm': { _: '0.75rem' },
-  'spacing-elements-xs': { _: '0.5rem' },
-  'spacing-elements-xxs': { _: '0.25rem' },
+  'spacing-xxl': { _: '2rem', sm: '4rem', xl: '6rem' },
+  'spacing-xl': { _: '1.5rem', sm: '2rem', xl: '3rem' },
+  'spacing-lg': { _: '1rem', sm: '1.5rem' },
+  'spacing-md': { _: '1rem' },
+  'spacing-sm': { _: '0.75rem' },
+  'spacing-xs': { _: '0.5rem' },
+  'spacing-xxs': { _: '0.25rem' },
 };
 
 export default { spacingsData };
