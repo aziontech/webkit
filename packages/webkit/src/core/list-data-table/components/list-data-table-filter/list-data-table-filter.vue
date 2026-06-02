d9f68a02c45ea86e93a3caa32a9deb5cc684219a
Robson Junior <robson.ga.junior@gmail.com>
2026-06-02 10:42:24 -0300

    fix: table actions IconButton sizes

--- packages/webkit/src/core/list-data-table/components/list-data-table-filter/list-data-table-filter.vue
+++ packages/webkit/src/core/list-data-table/components/list-data-table-filter/list-data-table-filter.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import PrimeButton from 'primevue/button'
+  import Button from '@aziontech/webkit/button'
   import Dropdown from 'primevue/dropdown'
   import OverlayPanel from 'primevue/overlaypanel'
   import { computed, ref } from 'vue'
@@ -114,10 +114,9 @@
         class="flex items-center justify-between px-6 py-4 border-b border-[var(--surface-border)]"
       >
         <h3 class="text-lg font-semibold">Filter</h3>
-        <PrimeButton
-          outlined
+        <Button
+          kind="outlined"
           icon="pi pi-times"
-          class="color-[var(--actionIconColor)]"
           @click="handleCancel"
         />
       </div>
@@ -157,14 +156,14 @@
       </div>
 
       <div class="flex justify-end gap-2 px-6 py-4 border-t border-[var(--surface-border)]">
-        <PrimeButton
+        <Button
           label="Cancel"
-          outlined
+          kind="outlined"
           @click="handleCancel"
         />
-        <PrimeButton
+        <Button
           label="Apply"
-          severity="secondary"
+          kind="secondary"
           @click="handleApply"
         />
       </div>
