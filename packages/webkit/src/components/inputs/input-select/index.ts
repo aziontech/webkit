8da4bfa621023e194825d70438a0986c9fe286dd
Isaque Böck <isaquebockdos@gmail.com>
2026-06-22 18:15:42 -0300

    fix: avoid TS4023 on InputSelect by typing sub-component augmentation explicitly

--- packages/webkit/src/components/inputs/input-select/index.ts
+++ packages/webkit/src/components/inputs/input-select/index.ts
@@ -4,12 +4,18 @@ import InputSelectGroup from './input-select-group/input-select-group.vue'
 import InputSelectOption from './input-select-option/input-select-option.vue'
 import InputSelectTrigger from './input-select-trigger/input-select-trigger.vue'
 
-const InputSelect = Object.assign(InputSelectRoot, {
-  Trigger: InputSelectTrigger,
-  Content: InputSelectContent,
-  Group: InputSelectGroup,
-  Option: InputSelectOption
-})
+interface InputSelectStatic {
+  Trigger: typeof InputSelectTrigger
+  Content: typeof InputSelectContent
+  Group: typeof InputSelectGroup
+  Option: typeof InputSelectOption
+}
+
+const InputSelect = InputSelectRoot as typeof InputSelectRoot & InputSelectStatic
+InputSelect.Trigger = InputSelectTrigger
+InputSelect.Content = InputSelectContent
+InputSelect.Group = InputSelectGroup
+InputSelect.Option = InputSelectOption
 
 export default InputSelect
 export { InputSelectContent, InputSelectGroup, InputSelectOption, InputSelectTrigger }
