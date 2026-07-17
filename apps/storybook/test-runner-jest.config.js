f66f5d4ca36cec5dea77f21d325c8959774c8bd9
Robson Júnior <robson.ga.junior@gmail.com>
2026-07-17 14:17:38 -0300

    test: removing foundations from visual tests execution

--- apps/storybook/test-runner-jest.config.js
+++ apps/storybook/test-runner-jest.config.js
@@ -1,19 +1,17 @@
 const { getJestConfig } = require('@storybook/test-runner')
 
 const defaultConfig = getJestConfig()
+const testsDenied = [
+  '/src/stories/_shared/',
+  '/src/stories/foundations/',
+  '/src/stories/templates/'
+]
 
-/**
- * Jest layer of the visual test runner (test-storybook picks this file up
- * automatically from the package root).
- */
 module.exports = {
   ...defaultConfig,
   testTimeout: 45000,
   testPathIgnorePatterns: [
     ...(defaultConfig.testPathIgnorePatterns ?? []),
-    // Regex patterns (not globs): any templates/ folder directly under
-    // src/stories/ or nested deeper in the stories tree.
-    '/src/stories/templates/',
-    '/src/stories/.*/templates/'
+    ...(testsDenied ?? [])
   ]
 }
