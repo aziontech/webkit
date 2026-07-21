f2706713b802f0427dab4010d8ae15fa9d6e853f
Gab <gabriel.mendonca@azion.com>
2026-07-21 15:47:50 -0300

    feat(webkit-sample): wire routes and sidebar for new console and site views
    Routes and sidebar navigation for Edge DNS, Object Storage, SQL Database, plus the /site marketing router redirect.

--- apps/webkit-sample/src/router.js
+++ apps/webkit-sample/src/router.js
@@ -3,17 +3,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AccountSettings from './components/AccountSettings.vue'
 import ApplicationDetail from './components/ApplicationDetail.vue'
 import Applications from './components/Applications.vue'
+import BucketBrowser from './components/BucketBrowser.vue'
 import CardBoxSaves from './components/CardBoxSaves.vue'
 import CheckInbox from './components/CheckInbox.vue'
 import CreateApplication from './components/CreateApplication.vue'
+import CreateSqlDatabase from './components/CreateSqlDatabase.vue'
 import CreateTeam from './components/CreateTeam.vue'
 import CreateWorkload from './components/CreateWorkload.vue'
+import CreateZone from './components/CreateZone.vue'
 import CreationCenter from './components/CreationCenter.vue'
 import Dashboard from './components/Dashboard.vue'
 import DeployTemplate from './components/DeployTemplate.vue'
 import DialogForm from './components/DialogForm.vue'
 import DrawerForm from './components/DrawerForm.vue'
 import DrawerItemGroups from './components/DrawerItemGroups.vue'
+import EdgeDns from './components/EdgeDns.vue'
+import EdgeDnsZoneDetail from './components/EdgeDnsZoneDetail.vue'
 import FormsIndex from './components/FormsIndex.vue'
 import Home from './components/Home.vue'
 import InPageForm from './components/InPageForm.vue'
@@ -22,10 +27,14 @@ import ItemGroupSettings from './components/ItemGroupSettings.vue'
 import LoginScreen from './components/LoginScreen.vue'
 import Marketplace from './components/Marketplace.vue'
 import NestedDrawer from './components/NestedDrawer.vue'
+import ObjectStorage from './components/ObjectStorage.vue'
 import Personalize from './components/Personalize.vue'
 import PersonalTokens from './components/PersonalTokens.vue'
 import Playground from './components/Playground.vue'
 import SignUp from './components/SignUp.vue'
+import LandingAzion from './components/site/LandingAzion.vue'
+import SqlDatabase from './components/SqlDatabase.vue'
+import SqlDatabaseDetail from './components/SqlDatabaseDetail.vue'
 import TemplateSettings from './components/TemplateSettings.vue'
 import Variables from './components/Variables.vue'
 import WorkloadDetail from './components/WorkloadDetail.vue'
@@ -33,6 +42,11 @@ import Workloads from './components/Workloads.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
+  // Segregated marketing router: landing-page examples that render in the
+  // sidebar-less SiteLayout (azion.com-style website nav + footer), separate
+  // from the console app shell used by every other route.
+  { path: '/site', redirect: '/site/home' },
+  { path: '/site/home', name: 'site-home', component: LandingAzion },
   { path: '/login', name: 'login', component: LoginScreen },
   { path: '/signup', name: 'signup', component: SignUp },
   { path: '/signup/verify', name: 'signup-verify', component: CheckInbox },
@@ -47,6 +61,14 @@ const routes = [
   { path: '/applications/new', name: 'applications-new', component: CreateApplication },
   { path: '/applications/:id', name: 'application-detail', component: ApplicationDetail },
   { path: '/variables', name: 'variables', component: Variables },
+  { path: '/edge-dns', name: 'edge-dns', component: EdgeDns },
+  { path: '/edge-dns/new', name: 'edge-dns-new', component: CreateZone },
+  { path: '/edge-dns/:id', name: 'edge-dns-zone-detail', component: EdgeDnsZoneDetail },
+  { path: '/object-storage', name: 'object-storage', component: ObjectStorage },
+  { path: '/object-storage/:bucket', name: 'bucket-browser', component: BucketBrowser },
+  { path: '/sql-database', name: 'sql-database', component: SqlDatabase },
+  { path: '/sql-database/new', name: 'sql-database-new', component: CreateSqlDatabase },
+  { path: '/sql-database/:id', name: 'sql-database-detail', component: SqlDatabaseDetail },
   { path: '/forms', name: 'forms', component: FormsIndex },
   { path: '/forms/in-page', name: 'forms-in-page', component: InPageForm },
   { path: '/forms/fields-separated', name: 'forms-fields-separated', component: TemplateSettings },
